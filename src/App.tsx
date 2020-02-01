eca48efdce413c45328c3d77c655c8f00d865472
Bradon Zhang <bradonzhang@gmail.com>
2020-02-01 16:58:52 -0500

    Initial commit?

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { AppState, StyleSheet, Button, View } from 'react-native';
+import { Pedometer } from 'expo-sensors';
+import styled from 'styled-components/native';
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager';
+import * as Battery from 'expo-battery';
+import * as IntentLauncher from 'expo-intent-launcher';
+
+const LargeText = styled.Text`
+  font-size: 48px;
+`;
+
+const MediumText = styled.Text`
+  font-size: 36px;
+`;
+
+// let steps = 0;
+TaskManager.defineTask('countSteps', () => {
+  if (AppState.currentState === 'active') {
+    console.log('activity');
+  } else {
+    console.log('loggers?');
+    alert('oh no you are not on the app');
+  }
+});
+
+export default function App() {
+  const [steps, setSteps] = useState(0);
+  const [active, setActive] = useState(true);
+
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     Battery.getBatteryLevelAsync()
+  //       .then(batteryLevel => setBatteryLevel(Math.round(100 * batteryLevel)));
+  //   }, 1000);
+  // });
+
+  useEffect(() => {
+    BackgroundFetch.registerTaskAsync('countSteps', {
+      stopOnTerminate: false,
+      startOnBoot: true,
+    });
+    Pedometer.watchStepCount(({ steps }) => {
+      setSteps(steps);
+    });
+    // setInterval(() => {
+    //     .then(res => console.log(res));
+    // }, 1000);
+  }, []);
+
+  useEffect(() => {
+    AppState.addEventListener('change', state => setActive(state === 'active'));
+    return () => AppState.removeEventListener('change', null);
+  }, []);
+
+  const handlePress = () => {
+    alert(`rahul's wish is my command`);
+  };
+
+  return (
+    <View style={styles.container}>
+      <LargeText>{steps} steps</LargeText>
+      <LargeText>{Math.floor(steps / 100)} <MediumText>SafeCoins™</MediumText></LargeText>
+      <Button title={'press me uwu'} onPress={handlePress} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
